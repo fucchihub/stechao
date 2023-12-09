af19cbb7c04ec1714008dcea8da63001d62345a3
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-12-09 07:08:07 +0000

    [Add]public/posts画像がない時の画像表示機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   belongs_to user
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/チベットスナギツネ.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_fill: [width, height]).processed
+  end
 end
