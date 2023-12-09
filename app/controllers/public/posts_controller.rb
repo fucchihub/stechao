5aa6ea3944a5d3266efb640b84239e5b773b1b54
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-12-09 06:54:48 +0000

    [Add]public/posts新規投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,19 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @post = Post.new
   end
-  
+
   def create
-    @post = Post.new
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = "#{params[:name].truncate(10)}を投稿しました！"
+      redirect_to post_path(@post.id)
+    else
+      flash[:error] = "投稿できませんでした..."
+      render :new
+    end
   end
 
   def index
@@ -18,10 +27,16 @@ class Public::PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
   end
-  
+
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :image, :caption, :quantity)
   end
 end
