b3d52689793fd6fd5f1337c1c2cec49d2fcac670
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-12-12 07:49:09 +0000

    [Add]public/favoritesアソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   def get_image(width, height)
     unless image.attached?
